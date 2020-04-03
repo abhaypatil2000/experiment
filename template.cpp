b1e1286880dbcea534e458e0ba3a2f0d68e29650
abhaypatil2000 <56753048+abhaypatil2000@users.noreply.github.com>
2020-04-04 02:26:56 +0530

    improvements
    1. ll for long
    2. more for loops
        a. fo, foi, foj, fok
        b. Fo, Foi, Foj, Fok
    3. ll mox is declared
    4. fill is still unsorted

--- template.cpp
+++ template.cpp
@@ -1,8 +1,15 @@
 #include<bits/stdc++.h>
+//template
 
-#define io ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+#define io ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL)
 #define fo(i, x) for (i = 0; i < x; i++)
+#define foi(x) for (i = 0; i < x; i++)
+#define foj(x) for (j = 0; j < x; j++)
+#define fok(x) for (k = 0; k < x; k++)
 #define Fo(i, a, b) for (i = a; i < b; i++)
+#define Foi(a, b) for (i = a; i < b; i++)
+#define Foj(a, b) for (j = a; j < b; j++)
+#define Fok(a, b) for (k = a; k < b; k++)
 #define pb(x) push_back(x)
 #define pf(x) push_front(x)
 #define mp(x, y) make_pair(x, y)
@@ -22,14 +29,15 @@
 #define sbsot(a) stable_sort(a.begin(), a.end())
 #define fox(a) for (auto x : a)
 #define foy(a, b) for (auto a : b)
+#define ll long long int
 #define deb(x) cout << #x << " " << x << endl;
 #define fills(a, b) for (auto ploopy = a.begin(); ploopy != a.end(); ploopy++) *ploopy = b;
 #define fill(a,v) memset(a, v, sizeof (a))
 #define fill(a, b) for (int ploopy = 0; ploopy < sizeof(a)/sizeof(a[0]); ploopy++) a[ploopy] = b;
 using namespace std;
-
+ll mox = 1e9 + 7;
 
 int main()
 {
-	
+	//template
 }
