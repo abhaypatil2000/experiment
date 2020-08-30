0e8e564c5db55fe7073d4f7d56578ccf0ab46674
Abhay Patil <56753048+abhaypatil2000@users.noreply.github.com>
2020-08-30 14:36:46 +0530

    Update Union_Find.cpp

--- Union_Find.cpp
+++ Union_Find.cpp
@@ -1,14 +1,14 @@
 struct UnionFind
 {
-	vector<int> v;
-	vector<int> rank;
-	UnionFind(int N)
+	vector<int> v; // to store the parent of current node. i.e, v[i] is the parent of i
+	vector<int> rank; // stores the rank of each node // rank = number of nodes below the current node
+	UnionFind(int N) // constructor to initialise N number of nodes, each with parent as itself and rank = 1. i.e., all nodes are disjoint
 	{
 		v.resize(N);
-		rank.resize(N, 1);
+		rank.resize(N, 1); // rank = 1 for all nodes
 		int i;
 		for (i = 0; i < N; i++)
-			v[i] = i;
+			v[i] = i; // parent[i] = i // parent of itself
 	}
 	void Union(int a, int b)
 	{
@@ -17,7 +17,7 @@ struct UnionFind
 		y = Find(b);
 		if (x == y)
 			return;
-		if (rank[x] > rank[y])
+		if (rank[x] > rank[y]) // if merging two disjoint sets, make the parent of bigger set as the ultimate parent of their union
 		{
 			v[y] = x;
 			rank[x] += rank[y];
@@ -33,6 +33,7 @@ struct UnionFind
 	{
 		if (v[x] == x)
 			return x;
-		return (v[x] = Find(v[x]));
+		return (v[x] = Find(v[x])); // path compression step.
+		// return Find(v[x]) // without path compression
 	}
 };
