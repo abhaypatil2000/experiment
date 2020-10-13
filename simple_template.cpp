82bff2c0d80384a6b3fb64cc2da0560a886dd5df
Abhay Patil <56753048+abhaypatil2000@users.noreply.github.com>
2020-10-14 04:35:44 +0530

    Update simple_template.cpp

--- simple_template.cpp
+++ simple_template.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 // #define int long long // remember to make constants use the LL suffix
 // int mox = 1e9 + 7;
+// #define maxheap(x) priority_queue<(x)>;
+// #define minheap(x) priority_queue<(x), vector<x>, greater<(x)>>;
+// date : 14/10/2020
+// numeric_limits
+// #define deb(x) cout << #x << " = " << x << endl;
+// #define printer(v) for (auto x : v) {cout << x << " ";} cout << endl;
 
 int32_t main()
 {
@@ -14,13 +20,17 @@ int32_t main()
 	cin.tie(0);
 	cout.tie(0);
 	int t, n, i, j, k, m, q;
+	string s;
 	t = 1;
 	cin >> t;
 	while (t--)
 	{
 		cin >> n;
 		m = n;
-		cin >> m;
+		// cin >> k;
+		// cin >> m;
+		// cin >> s;
+		// vector<vector<int>> grid(n, vector<int>(m, 0));
 		vector<int> v(n);
 		for (auto &x : v)
 			cin >> x;
